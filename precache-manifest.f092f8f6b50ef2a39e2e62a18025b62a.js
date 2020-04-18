857c28057e45682a6936f71b080523046f210c47
Kumar Shyam <Shyam.Kumar@lisec.com>
2020-04-18 20:34:34 +0400

    Updates

---
+++ precache-manifest.f092f8f6b50ef2a39e2e62a18025b62a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5df2fbfc28396c04973161ca38039331",
+    "url": "/covid19/index.html"
+  },
+  {
+    "revision": "33a5a08b6c2250ed4268",
+    "url": "/covid19/static/css/main.2d8f2bdd.chunk.css"
+  },
+  {
+    "revision": "83dd04e4a042ded1165f",
+    "url": "/covid19/static/js/2.a02b9fbc.chunk.js"
+  },
+  {
+    "revision": "eac4cf64bf573541fd6dba29c78393a0",
+    "url": "/covid19/static/js/2.a02b9fbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33a5a08b6c2250ed4268",
+    "url": "/covid19/static/js/main.6f088099.chunk.js"
+  },
+  {
+    "revision": "38ebcbfff9e1e0dfcfc0",
+    "url": "/covid19/static/js/runtime-main.8ae2d3fe.js"
+  }
+]);
