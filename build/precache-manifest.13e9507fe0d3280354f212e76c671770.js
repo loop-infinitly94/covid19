d347c8da98e99319131353cc251b7adb288092aa
Kumar Shyam <Shyam.Kumar@lisec.com>
2020-04-21 12:37:57 +0400

    '+latestbuild'

---
+++ build/precache-manifest.13e9507fe0d3280354f212e76c671770.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6accc069cd9283c883e33ad28c2de94",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b31e4feeaa6b91d8d76",
+    "url": "/static/css/2.b90ce945.chunk.css"
+  },
+  {
+    "revision": "d931c691cab48799419e",
+    "url": "/static/css/main.492e34a8.chunk.css"
+  },
+  {
+    "revision": "3b31e4feeaa6b91d8d76",
+    "url": "/static/js/2.c500bb76.chunk.js"
+  },
+  {
+    "revision": "d09ac505fec0e9130a1d5b85fb3fa6dc",
+    "url": "/static/js/2.c500bb76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d931c691cab48799419e",
+    "url": "/static/js/main.34ef241d.chunk.js"
+  },
+  {
+    "revision": "d9a2df771ef5bda69662",
+    "url": "/static/js/runtime-main.6b30d221.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.282ea096c8ea32fd196355057455ab16.js"
+  "/precache-manifest.13e9507fe0d3280354f212e76c671770.js"
 );
 
 self.addEventListener('message', (event) => {
