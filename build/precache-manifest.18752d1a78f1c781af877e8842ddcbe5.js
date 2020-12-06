cab4f4a64cfef3de4dc98988166e59d72130dcdb
Kumar Shyam <Shyam.Kumar@lisec.com>
2020-12-06 15:32:44 +0400

    '+ghpages'

---
+++ build/precache-manifest.18752d1a78f1c781af877e8842ddcbe5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8052a6d08b3b8f0cc823779b8ee23daa",
+    "url": "/covid19/index.html"
+  },
+  {
+    "revision": "3339274fec850ab482b5",
+    "url": "/covid19/static/css/2.b90ce945.chunk.css"
+  },
+  {
+    "revision": "7c7dbe7a2b41de1336fc",
+    "url": "/covid19/static/css/main.9b2c5032.chunk.css"
+  },
+  {
+    "revision": "3339274fec850ab482b5",
+    "url": "/covid19/static/js/2.93ed24b4.chunk.js"
+  },
+  {
+    "revision": "d09ac505fec0e9130a1d5b85fb3fa6dc",
+    "url": "/covid19/static/js/2.93ed24b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c7dbe7a2b41de1336fc",
+    "url": "/covid19/static/js/main.ab19d2a8.chunk.js"
+  },
+  {
+    "revision": "38ebcbfff9e1e0dfcfc0",
+    "url": "/covid19/static/js/runtime-main.8ae2d3fe.js"
+  }
+]);
